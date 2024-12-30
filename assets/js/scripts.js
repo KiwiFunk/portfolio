cc5cf6ec601c0707b430e1f49602caf03be8a915
Alex <alex_p97@live.com>
2024-12-30 17:28:28 +0000

    Added class to h1, reworked selectors to target new class

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,11 @@
 // Triggering gradient change on H1 with mouse event on .linkbutton
 document.querySelectorAll('.linkbutton').forEach(item => {
     item.addEventListener('mouseover', () => {
-        const heading = document.querySelector('.page-heading h1');
+        const heading = document.querySelector('#page-heading .gradient-heading');
         heading.classList.add('hover-gradient-' + item.id);
     });
     item.addEventListener('mouseout', () => {
-        const heading = document.querySelector('.page-heading h1');
+        const heading = document.querySelector('#page-heading .gradient-heading');
         heading.classList.remove('hover-gradient-git', 'hover-gradient-linkedin', 'hover-gradient-artstation');
     });
 });
