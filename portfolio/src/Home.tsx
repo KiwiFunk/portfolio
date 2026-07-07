18f1a6b9fb22f4478d38e1e3486c1213460fb38a
KiwiFunk <alex_p97@live.com>
2026-07-07 16:53:12 +0100

    create home.tsx

---
+++ portfolio/src/Home.tsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+
+function Home() {
+
+  return (
+    <div id="canvas-container">
+      <Canvas />
+    </div>
+  );
+
+}
+
+export default Home;
