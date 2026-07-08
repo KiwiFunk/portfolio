02a76014e0e795df06941c69409654cd3a2ac2d7
Alex Price <alex_p97@live.com>
2026-07-08 14:35:09 +0100

    remove suspense for now

--- portfolio/src/Scene.tsx
+++ portfolio/src/Scene.tsx
@@ -1,20 +1,17 @@
-import { Suspense } from "react";
 import { Center } from "@react-three/drei";
 import Model from "./Model";
 
 export default function Scene() {
   return (
     <>
-      {/* Basic lighting just to see the model's textures */}
+      {/* Basic Lights - Move to Lights.jsx for full  */}
       <ambientLight intensity={0.5} />
       <directionalLight position={[10, 10, 5]} intensity={1.5} />
 
-      {/* Suspense catches the model while it loads so the app doesn't crash */}
-      <Suspense fallback={null}>
-        <Center>
-          <Model />
-        </Center>
-      </Suspense>
+      {/* Use Suspense in future if adding loading state  */}
+      <Center>
+        <Model />
+      </Center>
     </>
   );
 }
