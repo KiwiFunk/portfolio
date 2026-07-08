3a54bc9c58da09d1b96b7b7f85ae6fe3e3e6ac4e
Alex Price <alex_p97@live.com>
2026-07-08 16:20:53 +0100

    create helper func for degrees -> radians

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -3,6 +3,9 @@ import { OrbitControls } from "@react-three/drei";
 import Scene from "./Scene";
 
 export default function App() {
+  // Helper func to convert degrees to radians
+  const degToRad = (deg) => deg * (Math.PI / 180);
+
   return (
     <div style={{ width: "100vw", height: "100vh", background: "#1a1a1a" }}>
       <Canvas
@@ -16,13 +19,12 @@ export default function App() {
       >
         <OrbitControls
           makeDefault
-          // Uses Radians. 180 degrees = pi. 360 degrees = 2pi
-          // Radians = Degrees * (pi / 180)
           // Up/Down rotation limits
-          minPolarAngle={Math.PI / 2.8}
-          maxPolarAngle={Math.PI / 2}
-          minAzimuthAngle={Math.PI / 6} // 180 / 6 = 30 degrees
-          maxAzimuthAngle={Math.PI / 3} // 180 / 3 = 60 degrees
+          minPolarAngle={degToRad(65)}
+          maxPolarAngle={degToRad(90)}
+          // Left/Right rotation limits
+          minAzimuthAngle={degToRad(30)}
+          maxAzimuthAngle={degToRad(60)}
           // use Zoom for ortho, distance for perspective
           minZoom={45}
           maxZoom={80}
